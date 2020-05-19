1632e871065e8f9035711254140bb9ffaa1dc587
Sahej Singh <sahejdeep@hotmail.com>
2020-05-19 00:45:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,3 +15,13 @@ function whileLoop(n) {
 }
 return "done";
 }
+
+function doWhileLoop(num) {
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+}
